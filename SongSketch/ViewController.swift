257722f8877ba8a831af79035671fc14c1870db8
spgarry <samuelpgarry@gmail.com>
2021-03-30 17:41:33 -0400

    Initial Commit

---
+++ SongSketch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SongSketch
+//
+//  Created by Samuel Garry on 3/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
